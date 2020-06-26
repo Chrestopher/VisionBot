a8e97cb7fb70fd46a709ed616c1bd810fe2fbd5f
Christopher Chaffin <clc8680@rit.edu>
2020-06-26 13:36:41 -0400

    Try fix path

--- generate_data.py
+++ generate_data.py
@@ -6,7 +6,7 @@ parent_path = os.path.join(os_project_path, os.pardir)
 
 
 if os.environ.get("bot_cli_key"):
-    os_project_path = os.path.join(os_project_path, os.pardir)
+    os_project_path = os.path.join(os_project_path, os.pardir)[:3]
     print(os_project_path)
 
 
