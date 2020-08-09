4ff9950977be40ecd77598c4d77897b3d3ab3353
Christopher Chaffin <clc8680@rit.edu>
2020-08-09 15:25:55 -0400

    Test verify

--- anime.py
+++ anime.py
@@ -6,12 +6,12 @@ def search(args):
         return 'https://i.imgur.com/4vAEt1j.png'
     if len(test) >= 3:
         character = "%20".join(args)
-        result = requests.get('https://api.jikan.moe/v3/search/character?q=' + character +'&limit=1')
+        result = requests.get('https://api.jikan.moe/v3/search/character?q=' + character +'&limit=1', verify=False)
         if result.status_code == 404:
             return "Your Character does not exist."
         result_id = result.json()['results'][0]['mal_id']
         print(result_id)
-        pictures_links = requests.get('https://api.jikan.moe/v3/character/' + str(result_id) + '/pictures')
+        pictures_links = requests.get('https://api.jikan.moe/v3/character/' + str(result_id) + '/pictures', verify=False)
         print(pictures_links.json())
         if len(pictures_links.json()['pictures']) == 0:
             return "No Pictures Available. Your Character has no pictures."
