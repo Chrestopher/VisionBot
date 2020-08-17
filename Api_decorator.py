4d4c181e769b9d0c4a5a62c4a29e14db4d83e093
justinthall <67346266+justinthall@users.noreply.github.com>
2020-08-16 22:28:48 -0400

    Api decorators (#34)
    * lets calls save their contents, and adds function to get those contents.
    
    * Update Api_decorator.py

---
+++ Api_decorator.py
@@ -0,0 +1,27 @@
+import json
+
+
+class api_call_saver():
+
+    def __init__(self, apiCall):
+        self.function = apiCall
+
+    def __call__(self, callParameter):
+
+        result = self.function(callParameter)
+        data = [result]
+        with open("apicalls/{}.json".format(callParameter), "w") as outfile:
+            json.dump(data, outfile)
+            print("{} has been saved to file".format(callParameter))
+
+
+def pull_from_json(query):
+    try:
+        with open("apicalls/{}.json".format(query)) as file:
+            data = json.load(file)
+            return data
+    except:
+        raise Exception("query not found")
+
+
+
